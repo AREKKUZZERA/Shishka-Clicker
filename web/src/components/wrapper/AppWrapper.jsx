004c1f4069e3ecca53d9b636b3779f4de1e8f2c2
AREKKUZZERA <arekkuzzera@gmail.com>
2026-04-23 19:20:31 +0300

    fix: Discord SDK

--- web/src/components/wrapper/AppWrapper.jsx
+++ web/src/components/wrapper/AppWrapper.jsx
@@ -3,7 +3,7 @@ import { AchievementToast } from '../ui/AchievementToast.jsx'
 import { DevConsole } from '../ui/DevConsole.jsx'
 import { EventToast } from '../ui/EventToast.jsx'
 import { StatsBar } from '../stats/StatsBar.jsx'
-import { Suspense, lazy, memo, useEffect, useSyncExternalStore } from 'react'
+import { memo, useEffect, useSyncExternalStore } from 'react'
 import { observer } from 'mobx-react-lite'
 import { Header } from '../header/Header.jsx'
 import { useNav } from '../../context/NavContext.jsx'
@@ -11,12 +11,7 @@ import { useSettingsVisuals } from '../../context/SettingsContext.jsx'
 import { useDiscordBoot } from '../../context/DiscordActivityContext.jsx'
 import { ScreenFallback } from './ScreenFallback.jsx'
 import { AmbientCanvas } from '../ui/AmbientCanvas.jsx'
-
-const DiscordRichPresenceBridge = lazy(() =>
-  import('../discord/DiscordRichPresenceBridge.jsx').then((module) => ({
-    default: module.DiscordRichPresenceBridge,
-  })),
-)
+import { DiscordRichPresenceBridge } from '../discord/DiscordRichPresenceBridge.jsx'
 
 export const loadClickerScreen = () => import('../clicker/ClickerScreen')
 export const loadShopScreen = () => import('../shop/ShopScreen')
@@ -253,11 +248,7 @@ export const AppWrapper = observer(function AppWrapper() {
   return (
     <div className="app-shell">
       <AppBackground visualEffectToggles={visualEffectToggles} />
-      {showBootScreen ? null : (
-        <Suspense fallback={null}>
-          <DiscordRichPresenceBridge />
-        </Suspense>
-      )}
+      {showBootScreen ? null : <DiscordRichPresenceBridge />}
 
       <div className="app-content">
         <Header />
